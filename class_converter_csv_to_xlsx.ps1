8f57a89143c3ad2c1f8d6e03513a01c82ee2d0b0
KatzMatz <katz.matz.dev@gmail.com>
2020-12-31 11:54:39 +0900

    first commit

--- class_converter_csv_to_xlsx.ps1
+++ class_converter_csv_to_xlsx.ps1
@@ -2,4 +2,100 @@
 
 class ConverterCsvToXlsx {
 
+    [void] convertCsvToXlsx([String] $csvPath, [String] $xlsxPath) {
+        if (!(Tets-Path $csvPath)) {
+            return
+        }
+
+        
+        try {
+            # Import the csv file
+            $data = Import-Csv $csvPath -Encoding UTF8
+            
+            # Open excel
+            $excel = New-Object -ComObject Excel.Application # Component Object model
+            
+            # Generate or Open a book
+            if (Test-Path $xlsxPath) {
+                # Open
+                $book = $excel.Workbooks.Open($xlsxPath)
+                
+                $preSheet = $book.Sheets($book.Sheets.Count)
+                $book.Worksheets.Add([System.Reflection.Missing]::Value, $preSheet)
+                $sheet = $book.Sheets($book.Sheets.Count)
+
+                $preSheet = $null
+            } else {
+                # Generate
+                $book = $excel.Workbooks.Add()
+                $sheet = $book.Sheets(1)
+            }
+
+            # Set sheet name
+            $csvName = Split-Path $csvPath -Leaf
+            $sheetName = $csvName.Replace(".csv", "")
+            $sheet.Name = $sheetName
+
+            # The cell position variables
+            $numRow = 1
+            $numColumn = 1
+
+            if ($data.Count -ne 0) {
+                # get header properties
+                $props = $data[0].psoobject.properties.name
+                
+                # set cell type as string and line style
+                $sheet.Range($sheet.Cells(1, 1), $sheet.Cells($data.Count, $props.Length)).NumberFormat = '@'
+                $sheet.Range($sheet.Cells(1, 1), $sheet.Celss($data.Count, $props.Length)).Borders.LineSyle = 1 # coutinuous
+                $sheet.Range($sheet.Cells(1, 1), $sheet.Celss($data.Count, $props.Length)).Borders.Weight = 2
+
+                # Output header row
+                foreach ($propItem in $props) {
+                    # set value
+                    $sheet.Cells.Item($numRow, $numColumn) = [String]$propItem
+                    #set line style
+                    
+                    # change position to right
+                    $numColumn++
+                }
+
+                # change position to bottom
+                $numRow++
+
+                # Output contents
+                foreach ($item in $data) {
+                    $numColumn = 1
+
+                    foreach ($propItem in $props) {
+                        $sheet.Cells.Item($numRow,$numColumn) = [String]$propItem
+                        #set line style
+                        # $sheet.Cells.Item($numRow, $numColumn).Borders.LineStyle = 1 # coutinuous
+                        # $sheet.Cells.Item($numRow, $numColumn).Borders.Weight = 2
+                        # change position to right
+                        $numColumn++
+                    }
+
+                    $numRow++
+                }
+            }
+
+            if (Test-Path $xlsxPath) {
+                $book.Save()
+            } else {
+                $book.SaveAs($xlsxPath)
+            }
+            
+            $excel.Quit()
+        } finally {
+            # GC
+            $sheet = $null
+            $book = $null
+            $excel = $null
+            [GC]::Collect()
+        }
+
+
+    }
+
 }
+
